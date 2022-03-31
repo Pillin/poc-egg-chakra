03f80e3627228b139eeab264e8aac346f3a9da44
Pillippa Ignacia Pérez Pons <ignacia.pp@gmail.com>
2022-03-31 15:24:14 -0300

    added responsive

--- pages/components/forms/DetailForm.tsx
+++ pages/components/forms/DetailForm.tsx
@@ -1,18 +1,23 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, useBreakpointValue } from "@chakra-ui/react";
 import Select from "../core/Select";
 import CheckBox from "../core/CheckBox";
 import Input from "../core/Input";
 import Button from "../core/Button";
 
 const DetailForm = () => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1 }) || 1;
   return (
     <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full" h="full">
-      <Input colspan={1} placeholder="John" label="First Name" />
-      <Input colspan={1} placeholder="Doe" label="Last Name" />
+      <Input colspan={colSpan} placeholder="John" label="First Name" />
+      <Input colspan={colSpan} placeholder="Doe" label="Last Name" />
       <Input colspan={2} placeholder="Blvd. Broken Dreams" label="Address" />
-      <Input colspan={1} placeholder="San Francisco" label="City" />
-      <Select colspan={1} label="Country" options={[]} />
-      <CheckBox colspan={1} defaultChecked label="Ship to Billing Address" />
+      <Input colspan={colSpan} placeholder="San Francisco" label="City" />
+      <Select colspan={colSpan} label="Country" options={[]} />
+      <CheckBox
+        colspan={colSpan}
+        defaultChecked
+        label="Ship to Billing Address"
+      />
       <Button colspan={2} label="Place Order" />
     </SimpleGrid>
   );
